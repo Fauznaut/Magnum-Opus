be3d3c41381ab5038900d59ac66a0c8cfbb1fd63
Fauznaut <fauznaut@gmail.com>
2017-03-21 16:00:40 -0400

    Work Optimization and footer structure
    created static elements folder for elements that appear on every page,
    created basic structure for a footer, tweaked navbar to an optimal
    amount of buttons, added some classes to optimize content structure

---
+++ Static_Elements/navbar.php
@@ -0,0 +1,14 @@
+<div class="col-lg-2 textAlignCenter">
+	<button id="HomeButton" class="roundedButton amethystButton">Home</button>
+	<button id="ChangeMachineButton" class="roundedButton mintGreenButton">Change Machine</button>
+	<button id="ClickMasterButton" class="roundedButton amethystButton">Click Master</button>
+	<button id="" class="roundedButton mintGreenButton">Coming Soon</button>
+	<button id="" class="roundedButton amethystButton">Coming Soon</button>
+	<button id="" class="roundedButton mintGreenButton">Coming Soon</button>
+	<button id="" class="roundedButton amethystButton">Coming Soon</button>
+	<button id="" class="roundedButton mintGreenButton">Coming Soon</button>
+	<button id="" class="roundedButton amethystButton">Coming Soon</button>
+	<button id="" class="roundedButton mintGreenButton">Coming Soon</button>
+	<button id="" class="roundedButton amethystButton">Coming Soon</button>
+	<button id="" class="roundedButton mintGreenButton">Coming Soon</button>
+</div>
--- index.php
+++ index.php
@@ -12,36 +12,12 @@
 <body>
 	<!-- AngularJS -->
 	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
-	<!-- Website Header -->
-    <div class="container-fluid header eggshell">
-        <div class="row">
-            <div class="col-lg-1 headerColumn darkGrey">
-            </div>
-            <div class="col-lg-1 headerColumn amethyst">
-            </div>
-            <div class="col-lg-8">
-                <h1 id="headerText" class="text-center">Math Website</h1>
-            </div>
-            <div class="col-lg-1 headerColumn amethyst">
-            </div>
-            <div class="col-lg-1 headerColumn darkGrey">
-            </div>
-        </div>
-    </div>
+	<!-- Header -->
+    <?php include 'Static_Elements/header.php';?>
 	<!-- Control Buttons and Page Content -->
-	<div class="container-fluid">
+	<div class="container-fluid contentContainer">
 		<div class="row">
-			<div class="col-lg-2 textAlignCenter">
-				<button id="HomeButton" class="roundedButton amethystButton">Home</button>
-				<button id="ChangeMachineButton" class="roundedButton mintGreenButton">Change Machine</button>
-				<button id="" class="roundedButton amethystButton">Coming Soon</button>
-				<button id="" class="roundedButton mintGreenButton">Coming Soon</button>
-				<button id="" class="roundedButton amethystButton">Coming Soon</button>
-				<button id="" class="roundedButton mintGreenButton">Coming Soon</button>
-				<button id="" class="roundedButton amethystButton">Coming Soon</button>
-				<button id="" class="roundedButton mintGreenButton">Coming Soon</button>
-				<button id="" class="roundedButton amethystButton">Coming Soon</button>
-			</div>
+			<?php include 'Static_Elements/navbar.php';?>
 			<div class="col-lg-10 pageContent">
 				<div class="row contentHeaderRow">
 					<div class="col-lg-12">
@@ -61,12 +37,12 @@
 			</div>
 		</div>
 	</div>
+	<!-- Footer -->
+	<?php include 'Static_Elements/footer.php';?>
 	<!-- JavaScript -->
 	<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.1.1.min.js" charset="utf-8"></script>
     <script src="JavaScript/FadeIn.js" charset="utf-8"></script>
 	<script src="JavaScript/Buttons.js" charset="utf-8"></script>
 </body>
-
-</html>
 
 </html>
